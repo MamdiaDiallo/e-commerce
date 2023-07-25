b74f2fcd7320d200a13aa6cf32aff8a810e7a17b
DIALLO Mamoudou (SNCF / DGA NUMERIQUE / e.SNCF Sol.DEA UPP PROC) <9409689Y@commun.ad.sncf.fr>
2023-07-25 14:07:56 +0200

    domain

--- backend/e-commerce/src/main/java/fr/sncf/ecommerce/users/domain/models/User.java
+++ backend/e-commerce/src/main/java/fr/sncf/ecommerce/users/domain/models/User.java
@@ -1,5 +1,27 @@
 package fr.sncf.ecommerce.users.domain.models;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class User {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate dateOfBirth;
+
+    private String email;
+
+    private LocalDateTime createdDateTime;
+
+    private UserRole role;
+
+    private String password;
 
 }
